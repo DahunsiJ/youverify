da773fd9b05cabad9c7f1cfc860baca81b10d314
DahunsiJ <justusdahunsi6.com>
2024-09-07 17:01:08 +0100

    backend

--- devSecOps/aks/terraformIAC/backend.tf
+++ devSecOps/aks/terraformIAC/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-backend-rg"
     storage_account_name = "yvterraformbackendstore"
-    container_name       = "aks_tfstate"
+    container_name       = "akstfstate"
     key                  = "aks_server.tfstate"
   }
 }
